ede2e77b1768362a9c9192dae6a244b25f754b5f
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-27 09:22:07 +0300

    fetch way

--- js/axios.js
+++ js/axios.js
@@ -3,5 +3,13 @@ console.log('axios.js file was loaded');
 
 // parsiusti ir iskonsolinti visus userius is
 // https://jsonplaceholder.typicode.com/users
+const url = 'https://jsonplaceholder.typicode.com/users';
 
-// fetch()
+fetch(url)
+  .then((resp) => resp.json())
+  .then((atsInJs) => {
+    console.log('atsInJs ===', atsInJs);
+  })
+  .catch((error) => {
+    console.warn('ivyko klaida:', error);
+  });
