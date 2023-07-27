577aa4ea3ee3d1aef157cdfec591dfe8c98482ff
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-27 10:20:17 +0300

    try catch

--- js/asyncAwait.js
+++ js/asyncAwait.js
@@ -17,9 +17,24 @@ function fetchUsers() {
 }
 // fetchUsers();
 async function fetchUsersA() {
-  // await - palaukti kol grys atsakymas ir tik tada vykdyti kita eilute
-  const resp = await fetch(url);
-  const atsInJs = await resp.json();
-  console.log('atsInJs ===', atsInJs);
+  try {
+    // await - palaukti kol grys atsakymas ir tik tada vykdyti kita eilute
+    const resp = await fetch(url);
+    const userArr = await resp.json();
+    const respPost = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const postsArr = await respPost.json();
+    console.log('userArr ===', userArr, 'posts', postsArr);
+  } catch (error) {
+    console.warn('FETCH await ivyko klaida:', error);
+  }
 }
 fetchUsersA();
+
+// su async await parasyt funkcija kuri grazina postus
+// https://jsonplaceholder.typicode.com/posts
+async function getPosts() {
+  const resp = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const dataInJs = await resp.json();
+  console.log('dataInJs ===', dataInJs.slice(0, 15));
+}
+getPosts();
