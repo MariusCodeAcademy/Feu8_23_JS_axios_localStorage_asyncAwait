9edb42dc810fdb7a698d7e214158c66b9327ae36
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-27 09:30:03 +0300

    axios get

--- js/axios.js
+++ js/axios.js
@@ -3,13 +3,28 @@ console.log('axios.js file was loaded');
 
 // parsiusti ir iskonsolinti visus userius is
 // https://jsonplaceholder.typicode.com/users
-const url = 'https://jsonplaceholder.typicode.com/users';
+const url = 'https://jsonplaceholder.typicode.com/users111';
 
 fetch(url)
-  .then((resp) => resp.json())
+  .then((resp) => {
+    console.log('FETCH resp ===', resp);
+    // if (!resp.ok) throw new Error('kazkas ne taip');
+    return resp.json();
+  })
   .then((atsInJs) => {
-    console.log('atsInJs ===', atsInJs);
+    console.log('FETCH atsInJs ===', atsInJs);
   })
   .catch((error) => {
-    console.warn('ivyko klaida:', error);
+    console.warn('FETCH ivyko klaida:', error);
+  });
+
+axios
+  .get(url)
+  .then((axiosResp) => {
+    console.log('axiosResp ===', axiosResp);
+    const users = axiosResp.data;
+    console.log('axiosResp users ===', users);
+  })
+  .catch((error) => {
+    console.warn('AXIOS ivyko klaida:', error);
   });
