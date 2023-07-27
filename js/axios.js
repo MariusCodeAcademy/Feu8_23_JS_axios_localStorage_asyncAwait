d6f5fd9e7b32bf98aceab13d58d0d2d7de0a5b21
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-27 09:35:44 +0300

    fetch post

--- js/axios.js
+++ js/axios.js
@@ -3,7 +3,7 @@ console.log('axios.js file was loaded');
 
 // parsiusti ir iskonsolinti visus userius is
 // https://jsonplaceholder.typicode.com/users
-const url = 'https://jsonplaceholder.typicode.com/users111';
+const url = 'https://jsonplaceholder.typicode.com/users';
 
 fetch(url)
   .then((resp) => {
---
+++ js/axiosPost.js
@@ -0,0 +1,24 @@
+'use strict';
+console.log('axiosPost.js file was loaded');
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+// create new post in
+const newPostObj = {
+  title: 'foo',
+  body: 'bar',
+  userId: 1,
+};
+// siusti fetch posta kad sukurti nauja posta
+fetch(url, {
+  method: 'POST',
+  headers: { 'Content-type': 'application/json' },
+  body: JSON.stringify(newPostObj),
+})
+  .then((resp) => resp.json())
+  .then((rezult) => {
+    console.log('rezult ===', rezult);
+  })
+  .catch((error) => {
+    console.warn('ivyko klaida:', error);
+  });
